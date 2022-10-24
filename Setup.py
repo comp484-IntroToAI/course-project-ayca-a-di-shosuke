41f93419b85955fdd61a1ad0318d950931f0a4fb
A'di Dust <adust@macalester.edu>
2022-10-24 16:16:47 -0500

    add setup

--- Setup.py
+++ Setup.py
@@ -1,9 +1,28 @@
+from ast import main
+from lib2to3.pgen2.tokenize import tokenize
+from mosestokenizer import *
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
 class Setup():
-    def __init__():
-        pass
+    def __init__(self):
+        self.tokenize = MosesTokenizer('en')
     
-    def readFile():
-        pass
+    def readFile(self, data_name = 'scientific_papers'):
+        self.train_set = tfds.load(data_name, split='train')
+            
     
-    def buildVocabulary():
-        pass
+    def buildVocabulary(self):
+        self.vocab = self.tokenize(self.train_set[0].abstract)
+        self.tokenize.close()
+        
+    def printVocabulary(self):
+        print(self.vocab)
+        
+
+       
+if __name__ == "__main__":
+    setup = Setup()
+    setup.readFile()
+    setup.buildVocabulary()
+    setup.printVocabulary()
