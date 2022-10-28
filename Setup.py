4664ded6e027746c2475c013eb8ca43e319c68ea
A'di Dust <adust@macalester.edu>
2022-10-28 10:16:26 -0500

    optimize pickle process

--- Setup.py
+++ Setup.py
@@ -26,16 +26,23 @@ class Setup():
             
     def buildVocabulary(self):
         print("building vocabulary")
+        self.write_list(["asfg"])
         self.vocab = []
         article_num = 0
         for article in self.train_set:
-            article_num = article_num + 1
             vocabu = str(article['article'])
             vocabu = vocabu.lower()
             vocabu = list(set(self.tokenize.tokenize(vocabu)))
             self.vocab.extend(vocabu)
-            self.vocab = list(set(self.vocab))
             print("article tokenized: " + str(article_num))
+            article_num = article_num + 1
+            if article_num % 1000 == 0:
+                print("saving article " + str(article_num))
+                current_list = self.read_list()
+                current_list.extend(self.vocab)
+                current_list = list(set(current_list))
+                self.write_list(current_list)
+                self.vocab = []
         self.write_list(self.vocab)
         
     def printVocabulary(self):
