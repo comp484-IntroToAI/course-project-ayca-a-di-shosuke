5507c3d9338d8bdd1e2e865892320e6e3a345ed1
A'di Dust <adust@macalester.edu>
2022-11-08 02:31:50 -0600

    create method to filter out unnecessary words

--- Setup.py
+++ Setup.py
@@ -2,6 +2,7 @@ from sacremoses import MosesTokenizer
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import pickle
+import regex as re
 
 
 class Setup():
@@ -9,14 +10,14 @@ class Setup():
         self.tokenize = MosesTokenizer()
         
     # write list to binary file
-    def write_list(self, a_list):
-        with open('vocab', 'wb') as fp:
+    def write_list(self, a_list, file_name):
+        with open(file_name, 'wb') as fp:
             pickle.dump(a_list, fp)
             print('Done writing list into a binary file')
 
     # Read list to memory
-    def read_list(self):
-        with open('vocab', 'rb') as fp:
+    def read_list(self, file_name):
+        with open(file_name, 'rb') as fp:
             n_list = pickle.load(fp)
             return n_list
     
@@ -26,7 +27,7 @@ class Setup():
             
     def buildVocabulary(self):
         print("building vocabulary")
-        self.write_list([])
+        self.write_list([], "vocab")
         self.vocab = []
         article_num = 0
         for article in self.train_set:
@@ -43,16 +44,33 @@ class Setup():
         self.saveWords()
         
     def saveWords(self):
-        current_list = self.read_list()
+        current_list = self.read_list("vocab")
         current_list.extend(self.vocab)
         current_list = list(set(current_list))
-        self.write_list(current_list)
+        self.write_list(current_list, "vocab")
         
     def printVocabulary(self):
         print("fetching vocabulary")
-        print(len(self.read_list()))
+        # print(self.read_list("vocab")[2000:2100]/)
+        print("filtered")
+        print(len(self.read_list("filtered")))
+        print(self.read_list("filtered"))
+    
+    def filter_words_characters(self):
+        allowed_chars = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'-")
+        words = self.read_list("filtered")
+        new_words = []
+        x = 0 
+        for word in words:
+            validation = set((word))
+            if validation.issubset(allowed_chars):
+                new_words.append(word)
+            x = x + 1
+            print(str(x))
+        self.write_list(new_words, "filtered")
 
         
 if __name__ == "__main__":
     setup = Setup()
+    # setup.filter_words_characters()
     setup.printVocabulary()
