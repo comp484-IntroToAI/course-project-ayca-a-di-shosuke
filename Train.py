a57c20c74fd7bce5cbd66edcb278c7f6bcfccf12
aarbay <34741065+aarbay@users.noreply.github.com>
2022-11-20 22:04:18 -0600

    progress, but couldn't test

---
+++ Train.py
@@ -0,0 +1,111 @@
+from sacremoses import MosesTokenizer, MosesDetokenizer
+from statistics import mean
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import pickle
+import random
+import regex as re
+
+
+class Train():
+    def __init__(self):
+        self.mt = MosesTokenizer()
+        self.md = MosesDetokenizer()
+
+    def tokenize_sample(self):
+        
+        for article in self.train_set:
+            self.get_sentences(article)
+            
+    def create_individuals(self, article):
+        words = self.mt.tokenize(article['article'].lower())
+        word_weights = self.get_word_weights(words) 
+
+        sentences, sentence_weights = self.get_sentences(words, word_weights)
+
+        # TODO: for each sentence, create an individual with weight_attr sentence_weights[i]
+
+
+    def get_word_weights(self, words):
+        vocab = self.read_list("filtered")
+        weights = self.read_list("weights")
+
+        weights_to_return = []
+
+        for word in words:
+            i = vocab.index(word)
+            weight = weights[i]
+            weights_to_return.append(weight)
+
+        return weights_to_return
+
+    def get_sentences(self, words, word_weights):
+        sentences = []
+        sentence_weights = []
+
+        start_i = 0
+
+        while True:
+            try:
+                end_i = words.index('.', start_i)
+                
+                sentence = self.md.detokenize(words[start_i:end_i+1])
+                sentences.append(sentence)
+
+                sentence_weight = mean(word_weights[start_i:end_i+1])
+                sentence_weights.append(sentence_weight)
+
+                start_i = end_i+1
+                
+            except ValueError:
+                break
+
+        return sentences, sentence_weights
+
+    # write list to binary file
+    def write_list(self, a_list, file_name):
+        with open(file_name, 'wb') as fp:
+            pickle.dump(a_list, fp)
+            print('Done writing list into a binary file')
+
+    # Read list to memory
+    def read_list(self, file_name):
+        with open(file_name, 'rb') as fp:
+            n_list = pickle.load(fp)
+            return n_list
+    
+    def readFile(self, data_name = 'scientific_papers'):
+        print("Reading Data")
+        self.train_set = tfds.load(data_name, split='train') # delete take for whole training set
+
+    def get_filtered_vocabulary(self, start_index=0, end_index=-1):
+        filtered_list = self.read_list("filtered")
+
+        if end_index == -1:
+            end_index = len(filtered_list)
+
+        return filtered_list[start_index : end_index]
+
+    def get_weights(self, start_index=0, end_index=-1):
+        weights = self.read_list("weights")
+
+        if end_index == -1:
+            end_index = len(weights)
+
+        return weights[start_index : end_index]
+
+        
+if __name__ == "__main__":
+    train = Train()
+
+    ## setup.filter_words_characters()
+    # print(setup.get_filtered_vocabulary(0, 10))
+
+    ## setup.create_parallel_weight_list()
+    # print(setup.get_weights(0, 10))
+    
+    # train.readFile()
+    # train.tokenize_sample()
+
+
+
