76a33839452dfc6e44c95430b5acdbc201ae7d0c
A'di Dust <adust@macalester.edu>
2022-12-03 11:55:58 -0600

    Changed setup file to be more efficient/helpful

--- Setup.py
+++ Setup.py
@@ -1,102 +1,85 @@
-# from sacremoses import MosesTokenizer
-# import tensorflow as tf
-# import tensorflow_datasets as tfds
+# This file builds a vocab dictionary into a pickle file. The vocab consists
+# of alphabetic words and corresponding random weights between 0 and 1. 
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
 import pickle
 import random
-# import regex as re
+from collections import deque
+import re
 
 
 class Setup():
     def __init__(self):
-        # self.tokenize = MosesTokenizer()
-        pass
+        self.WORD = re.compile(r'\w+')
 
     # write list to binary file
-    def write_list(self, a_list, file_name):
+    def write_list(self, a_list, file_name = 'vocab_dict'):
         with open(file_name, 'wb') as fp:
             pickle.dump(a_list, fp)
             print('Done writing list into a binary file')
 
     # Read list to memory
-    def read_list(self, file_name):
+    def read_list(self, file_name = 'vocab_dict'):
         with open(file_name, 'rb') as fp:
             n_list = pickle.load(fp)
             return n_list
     
-    def readFile(self, data_name = 'scientific_papers'):
+    # read training set from tfds 
+    def read_papers_file(self, data_name = 'scientific_papers'):
         print("Reading Data")
-        self.train_set = tfds.load(data_name, split='train') # delete take for whole training set
-            
-    def buildVocabulary(self):
-        print("building vocabulary")
-        self.write_list([], "vocab")
-        self.vocab = []
-        article_num = 0
-        for article in self.train_set:
-            vocabu = str(article['article'])
-            vocabu = vocabu.lower()
-            vocabu = list(set(self.tokenize.tokenize(vocabu)))
-            self.vocab.extend(vocabu)
-            print("article tokenized: " + str(article_num))
-            article_num = article_num + 1
-            if article_num % 1000 == 0:
-                print("saving article " + str(article_num))
-                self.saveWords()
-                self.vocab = []
-        self.saveWords()
-        
-    def saveWords(self):
-        current_list = self.read_list("vocab")
-        current_list.extend(self.vocab)
-        current_list = list(set(current_list))
-        self.write_list(current_list, "vocab")
+        self.train_set = tfds.load(data_name, split='train')
     
-    def filter_words_characters(self):
+    # setup files and training data for building vocabulary
+    def setup_files(self):
+        self.read_papers_file()
+        self.write_list({}, "vocab_dict")
+        self.list_set = list(self.train_set)
+        
+    # creates a vocabulary from the scientific papers dataset           
+    def buildVocabulary(self):
+        self.setup_files()
+        words = {}
+        for article in self.list_set:
+            article = str(article['article']).lower() # convert to lower
+            tokens = self.regTokenize(article) # tokenize
+            tokens = self.filter_all_words(tokens) # filter out non-words
+            words.update(dict(zip(tokens, self.create_random_weights(len(tokens))))) # create weights
+        self.write_list(words, 'vocab_dict')
+        
+    # regex tokenizer since it is much faster than others
+    def regTokenize(self, text):
+        words = self.WORD.findall(text)
+        return words
+        
+    # filter out words with numbers and unlikely characters
+    def filter_words(self, word):
         allowed_chars = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'-")
-        words = self.read_list("filtered")
-        new_words = []
-        x = 0 
-        for word in words:
-            validation = set((word))
-            if validation.issubset(allowed_chars):
-                new_words.append(word)
-            x = x + 1
-            print(str(x))
-        self.write_list(new_words, "filtered")
-
-    def create_parallel_weight_list(self):
-        vocab = self.read_list("filtered")
-        weights = []
-
-        for word in vocab:
+        validation = set((word))
+        if validation.issubset(allowed_chars):
+            return word
+    
+    # loop through words and filter
+    def filter_all_words(self, words):
+        new_words = list(filter(self.filter_words, words))
+        return new_words
+    
+    # create weights between 0-1
+    def create_random_weights(self, number):
+        weights = deque()
+        for num in range(number):
             weights.append(random.random())
-
-        self.write_list(weights, "weights")
-
-    def get_filtered_vocabulary(self, start_index=0, end_index=-1):
-        filtered_list = self.read_list("filtered")
-
-        if end_index == -1:
-            end_index = len(filtered_list)
-
-        return filtered_list[start_index : end_index]
-
-    def get_weights(self, start_index=0, end_index=-1):
-        weights = self.read_list("weights")
-
-        if end_index == -1:
-            end_index = len(weights)
-
-        return weights[start_index : end_index]
+        return weights
+    
+    # return a printable version of the created vocab file
+    def read_vocab_dict(self):
+        vocab = self.read_list("vocab_dict")
+        return len(vocab)
 
         
 if __name__ == "__main__":
     setup = Setup()
-
-    # setup.filter_words_characters()
-    print(setup.get_filtered_vocabulary(0, 10))
-
-    # setup.create_parallel_weight_list()
-    print(setup.get_weights(0, 10))
+    # setup.buildVocabulary() # takes 40 minutes to run 
+    print(setup.read_vocab_dict())
 
 
